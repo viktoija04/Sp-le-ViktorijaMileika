99ecdd6b23d05c6d069c17aa823270d88d1e3f51
viktoija04 <vika.mileika@gmail.com>
2022-06-02 21:44:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const buttoneElements = document.querySelectorAll('button');
 let row = 1;
 let letter =1;
-const wordElements = document.querySelectorAll('word-row');
+const wordElements = document.querySelectorAll('.word-row');
 
 buttoneElements.forEach((eliment) => {
     eliment.addEventListener('click' , function() {
-        keypress(element.atttributes{"data-key"}.value)
+        keypress(element.atttributes["data-key"].value)
     });
 });
 
 
-function populateWord(key) (
+function populateWord(key) {
     wordElements[row - 1].querySelectorAll('.word')[letter - 1].innerText = key;
-)
+}
 
 function keypress(key) {
 if (key,toLowerCase() === 'enter') {
    // enterWord();
-}else if (key,toLowerCase() === 'del') {
+} else if (key,toLowerCase() === 'del') {
    // deleteLetter();
 } else {
    populateWord(key);
