fc1a25fc8e162a9ad86a1d4eb3bacb0f2cac1071
viktoija04 <vika.mileika@gmail.com>
2022-06-15 22:14:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttoneElements = document.querySelectorAll('button');
 let row = 1;
 let letter =1;
+const wordForTheDay ='Shout';
 const wordElements = document.querySelectorAll('.word-row');
 
 buttoneElements.forEach((eliment) => {
@@ -9,9 +10,36 @@ buttoneElements.forEach((eliment) => {
     });
 });
 
-
 function populateWord(key) {
+    if (letter < 6) {
     wordElements[row - 1].querySelectorAll('.word')[letter - 1].innerText = key;
+    letter += 1;
+    }
+}
+
+function checkWord() {
+    const letterElements = wordElements[row - 1].querySelectorAll('.word');
+
+    letterElements.forEach((element) => {
+        const wordForTheDay ='Shout';
+        const indexOfLetterInWordOfTheDay = wordForTheDay.toLowerCase().indexOf(element.innerText.toLowerCase());
+
+        if (indexOfLetterInWordOfTheDay === index) {
+            element.classList.add('word-green');
+        } else if(indexOfLetterInWordOfTheDay > 0) {
+            element.classList.add('word-yellow');
+        } else {
+            element.classList.add('word-grey');
+        }
+    });
+}
+
+function enterWord() {
+    if (letter < 6) {
+    alert ('not enough letters');
+    } else {
+        checkWord();
+    }
 }
 
 function keypress(key) {
@@ -25,4 +53,3 @@ if (key,toLowerCase() === 'enter') {
 
 }
 
-Permissions-Policy: interest-cohort=(false)
